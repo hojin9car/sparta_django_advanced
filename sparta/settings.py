434a0948497b5ec431333909fa21c7e241f6bc87
hojin9car <hojin9car@gmail.com>
2022-08-18 12:38:55 +0900

    connect mysql and migrate

--- sparta/settings.py
+++ sparta/settings.py
@@ -13,6 +13,8 @@ from pathlib import Path
 from typing import List  # 타입힌트 불러오기.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import pymysql
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -72,14 +74,18 @@ WSGI_APPLICATION = "sparta.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+pymysql.install_as_MySQLdb()
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "sparta",
+        "USER": "root",
+        "PASSWORD": "1111",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
